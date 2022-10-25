94c8b7415610280b6ea88d6866404713ef363d68
Dmitry Rudenko <dimarudenk@gmail.com>
2022-10-25 16:50:19 +0300

    фикс

--- src/layouts/Logo.js
+++ src/layouts/Logo.js
@@ -2,7 +2,7 @@ import {NavHashLink} from "react-router-hash-link";
 
 const Logo = () => {
     return (
-        <NavHashLink to="/">
+            <NavHashLink to={`/`}>
             <img className="rounded-circle" height={73} width={73}
                  src="https://sun9-63.userapi.com/s/v1/if1/_86rarat6d7LPOE5G6LfVegJuItSP73RjyPkwFpimEMdzEdshZG8VighmXowAFCf3YFTnvUY.jpg?size=100x100&quality=96&crop=20,20,503,503&ava=1"
                  alt={"logo"}/>
