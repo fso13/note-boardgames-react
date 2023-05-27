3c521e50a4edbf57ab9813882bef6ab08681d5eb
Dmitry Rudenko <dimarudenk@gmail.com>
2023-05-27 12:30:32 +0300

    141-2

--- src/views/ui/Notes.js
+++ src/views/ui/Notes.js
@@ -19,18 +19,23 @@ const Notes = () => {
                 {NotesJson.map((nt, index) => (
                     <Col sm="6" lg="6" xl="3" key={index}>
                         <Card>
-                            {nt.files != null ?
-                                <UncontrolledCarousel
-                                    className="img-fluid"
-                                    interval={false}
-                                    items={nt.files.map((nt, index) => {
-                                            return {
-                                                key: index,
-                                                src: nt
-                                            }
-                                        }
-                                    )}
-                                /> : <div/>
+                        {nt.files != null ?
+                        <div style={{position: 'relative'}}>
+                                        <div>{nt.isExtension ? <span style={{
+                                            position: 'absolute',
+                                            top: '8px',
+                                            color: "#ffffff",
+                                            backgroundColor: "#3db13d"
+                                        }}>дополнение</span> : ""}</div>
+                                        <div className="card-img" style={{
+                                            backgroundSize: "cover",
+                                            backgroundPosition: "center",
+                                            paddingBottom: "100%",
+                                            width: "100%",
+                                            backgroundImage: `url(${nt.files[0]})`
+                                        }}>
+                                        </div>
+                                    </div>: <div/>
                             }
 
                             {/*{nt.files != null ?*/}
