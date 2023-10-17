6ead47d55cdcbd056f606e7a50880bad9e31e57d
dimarudenk <dimarudenk@gmail.com>
2023-10-18 01:47:22 +0300

    google chart

--- src/views/ui/Chart.js
+++ src/views/ui/Chart.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
 import NotesJson from "../../static/note.json";
-import * as CanvasJSReact from "canvasjs-react-charts";
-import "./Chart.scss";
+import { Chart } from "react-google-charts";
 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+export const options = {
+
+    isStacked: "true",
+            bar: {
+              groupWidth: "85%"
+            },
+    height: "2500",
+    chartArea: { width: "50%", height: "2500" },
+    title: "Сыгранные партии",
+    legend: { position: "none" },
+    hAxis: {
+      title: "",
+      minValue: 0,
+    },
+    vAxis: {
+        textStyle : {
+            fontSize: 18 // or the number you want
+        },
+      title: "",
+    },
+  };
+
+
 
 export default class Button extends Component {
 
@@ -16,48 +36,24 @@ export default class Button extends Component {
         });
 
         let result = ListGameName.reduce((acc, e) => acc.set(e.title, (acc.get(e.title) || 0) + e.count), new Map());
+        let data =  [["Игра", ""]].concat(Array.from(result.entries()).sort((a, b) => b[1] - a[1]))
 
-        const options = {
-            height: 1000,
-            indexLabelFontSize: 15,
-            animationEnabled: true,
-            theme: "light2",
-            title: {
-                fontSize: 20,
-                text: "Количество сессий"
-            },
-            axisY2: {
-                labelFontSize: 15,
-                interval: 1
-            },
-            axisX2: {
-                labelFontSize: 0,
-            },
-            dataPointWidth: 13,
-            data: [{
-                axisYType: "secondary",
-                axisXType: "secondary",
-                type: "bar",
 
-                dataPoints: Array.from((Array.from(result.entries()).sort((a, b) => a[1] - b[1])).map((item) => {
-                    return {y: item[1], indexLabel: item[0], indexLabelFontSize: 15}
-                }))
-            }]
-        }
+
+        console.log(data)
 
         return (
             <div>
                 
-                <div className={"chart"}>
-                    <CanvasJSChart options={options} className={"chart"}
-                    />
-                </div>
+                    <Chart
+                    chartType="BarChart"
+                    width="100%"
+                    height="400px"
+                    data={data}
+                    options={options}
+    />
             </div>
 
         );
-    }
-
-    addSymbols(e) {
-        return CanvasJS.formatNumber(e.value);
     }
 }
